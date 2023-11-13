753871366b198abacafe8f003bdb8ecc3ccccc58
Mohamed Sami <srmf2025@gmail.com>
2023-11-13 19:45:42 +0530

    feat: ner api

--- ner/server.py
+++ ner/server.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import spacy
 
 app = FastAPI()
+nlp = spacy.load("model/model-best")
+
+
+class Task(BaseModel):
+    input: str
+
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"success": True, "message": "API is running"}
+
+
+@app.post("/ner")
+def ner(task: Task):
+    doc = nlp(task.input)
+    return {"success": True, "data": list(map(lambda x: {"text": x.text, "type": x.label_}, doc.ents))}
